a0795c667d5b5de5574a372c5b42e8b24e12a71b
cat <calvine.le@gmail.com>
2020-03-01 10:53:22 -0500

    data loader

--- main.py
+++ main.py
@@ -20,6 +20,22 @@ from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
+# Parser
+parser = argparse.ArgumentParser(description='GWR CIFAR10 Training')
+parser.add_argument('--lr', default=0.05, type=float, help='learning rate')
+parser.add_argument('--batch-size-train', default=3, type=int, help='batch size train')
+parser.add_argument('--num-iteration', default=20, type=int, help='iteration to train NAS')
+parser.add_argument('--num-epoch', default=1, type=int, help='number of epochs')
+parser.add_argument('--num-flow', default=10, type=int, help='number of flows')
+parser.add_argument('--window-size', default=3, type=int, help='sliding window size for time series data')
+args = parser.parse_args()
+
+if (torch.cuda.is_available() == 'True'):
+    device = 'cuda'
+else:
+    device = 'cpu'
+    
+
 # Load data
 filename = "daily-min-temperatures.csv"
 rows = LoadData(filename)
@@ -28,3 +44,25 @@ rows = LoadData(filename)
 data = np.asarray(rows)
 temp = data[:,1]
 sliding_data = window(temp, args.window_size)
+
+train_data = []
+for value in sliding_data:  
+    train_data = np.append(train_data, value)
+
+# dataloader for neural network training
+train_data = train_data.astype(float)
+trainloader = DataLoader(train_data, batch_size=args.batch_size_train, shuffle=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
